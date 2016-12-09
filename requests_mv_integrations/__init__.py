e4aeb2ea5cf67f1c69e71d0166fa51b50167a7f7
Jeff Tanner <jefft@tune.com>
2016-12-09 14:58:26 -0800

    [MVI-769] (requests-mv-integrations) Update package version post merge

--- requests_mv_integrations/__init__.py
+++ requests_mv_integrations/__init__.py
@@ -4,8 +4,8 @@
 #  @namespace requests_mv_integrations
 
 __title__ = 'requests-mv-integrations'
-__version__ = '0.1.7'
-__build__ = 0x000107
+__version__ = '0.1.8'
+__build__ = 0x000108
 __version_info__ = tuple(__version__.split('.'))
 
 __author__ = 'jefft@tune.com'
